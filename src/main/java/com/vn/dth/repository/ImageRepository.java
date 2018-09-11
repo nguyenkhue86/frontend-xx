9ef15a2d3681046e077f13ab24cf0aec8153158e
nhvi1 <havihcm@gmail.com>
2018-09-11 09:44:52 +0700

    delete all files

--- src/main/java/com/vn/dth/repository/ImageRepository.java
+++
@@ -1,9 +0,0 @@
-package com.vn.dth.repository;
-
-import com.vn.dth.entity.ImageEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ImageRepository extends JpaRepository< ImageEntity, Long> {
-}
--- src/main/java/com/vn/dth/repository/ProductReponsitory.java
+++
@@ -1,9 +0,0 @@
-package com.vn.dth.repository;
-
-import com.vn.dth.entity.ProductEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductReponsitory extends JpaRepository< ProductEntity, Long> {
-}
