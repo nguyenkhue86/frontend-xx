b8cb2ad344ad1622c98be5463f1a0b6f83746947
nhvi1 <havihcm@gmail.com>
2018-09-13 11:34:02 +0700

    13/9 : fix responsive

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, HostListener, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
 import {CookieService} from 'ngx-cookie-service';
+import {e} from "@angular/core/src/render3";
 
 @Component({
   selector: 'app-main',
@@ -8,6 +9,8 @@ import {CookieService} from 'ngx-cookie-service';
   styleUrls: ['./main.component.css']
 })
 export class MainComponent implements OnInit {
+
+  status_nav: Boolean;
 
   constructor(private router: Router,
               private cookieService: CookieService) {  }
@@ -24,10 +27,19 @@ export class MainComponent implements OnInit {
     this.userName = '';
     this.router.navigate(['login']);
   }
-  logaaa(event) {
-    console.log(event);
-  }
+  public innerWidth: any;
+
   ngOnInit() {
+    this.innerWidth = window.innerWidth;
+    if (this.innerWidth < 720) {
+      this.status_nav = false;
+    } else {
+      this.status_nav =true;
+    }
+  }
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    this.innerWidth = window.innerWidth;
   }
 
 }
